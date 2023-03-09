2063defc6b0538acef97890aeddea892d3f53a21
Tarek <tarek.dali@gritacademy.se>
2023-03-09 10:29:14 +0100

    Created a JSON object and got the data from it.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is an example!");
 
+        //Skapa ett JSON Object
+        JSONObject jsonOb = new JSONObject();
+
+        //Spara värden i JSON Object
+        jsonOb.put("namn", "Tarek");
+        jsonOb.put("age", 34);
+
+        //Skriva ut värden
+
+        System.out.println("Mitt namn är: " + jsonOb.get("namn"));
+        System.out.println("Jag är " + jsonOb.get("age") + " år gammal.");
     }
 }
